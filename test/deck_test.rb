921e0ff5ea2ae345fa72e97062b0cd49c8da7965
Jordan Whitten <jordanwa1947@gmail.com>
2018-08-18 10:50:43 -0700

    new deck class and count method added as well as tests for both

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,1 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/guess'
+require './lib/card'
+require './lib/deck'
+require 'pry'
+class DeckTest < Minitest::Test
+  def setup
+    @card_1 = Card.new("3","Hearts")
+    @card_2 = Card.new("4", "Clubs")
+    @card_3 = Card.new("5", "Diamonds")
+    @deck = Deck.new([@card_1, @card_2, @card_3])
+  end
 
+  def test_that_deck_now_holds_the_instantiated_cards
+    expected = [@card_1, @card_2, @card_3]
+    actual = @deck.cards
+    assert_equal expected, actual
+  end
+
+  def test_that_the_count_method_returns_the_array_length
+    expected = 3
+    actual = @deck.cards.length
+    assert_equal expected, actual
+  end
+end
