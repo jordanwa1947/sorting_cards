745272d3da1eb2c0a83bc305873a5fc9d72a91e8
Jordan Whitten <jordanwa1947@gmail.com>
2018-08-18 07:03:06 -0700

    new guess class and tests for attr_reader methods

--- test/guess_test.rb
+++ test/guess_test.rb
@@ -1,1 +1,23 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/guess'
+require './lib/card'
 
+class GuessTest < Minitest::Test
+  def setup
+    @card = Card.new("Ace", "Spades")
+    @guess = Guess.new("10 of Hearts", @card)
+  end
+
+  def test_a_guess_is_given
+    expected = "10 of Hearts"
+    actual = @guess.response
+    assert_equal expected, actual
+  end
+
+  def test_an_instance_of_the_card_class_was_passed
+    expected = Card
+    actual = @guess.card
+    assert_instance_of expected, actual
+  end
+end
