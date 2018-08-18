50562637976527aaac79a8e1450290b9b49904ee
Jordan Whitten <jordanwa1947@gmail.com>
2018-08-17 17:55:01 -0700

    updated test files, added new tests to card_test

---
+++ test/card_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+
+class CardTest < Minitest::Test
+  def setup
+    @card = Card.new("Ace", "Spades")
+  end
+
+  def test_that_the_card_has_a_value
+    expected = "Ace"
+    actual = @card.value
+    assert_equal expected, actual
+  end
+
+  def test_that_the_card_has_a_suit
+    expected = "Spades"
+    actual = @card.suit
+    assert_equal expected, actual
+  end
