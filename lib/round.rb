2a23d9ad93b5b51095141be91d9a884fd5a1676b
Jordan Whitten <jordanwa1947@gmail.com>
2018-08-18 17:44:55 -0700

    two new features added to get and record guesses as well as testing for both

--- lib/round.rb
+++ lib/round.rb
@@ -3,12 +3,20 @@ require './lib/guess'
 require './lib/card'
 require 'pry'
 class Round
-  attr_reader :deck
+  attr_reader :deck, :guesses
   def initialize(deck)
     @deck = deck
+    @guesses = []
   end
 
-  def guesses
-    guesses = []
+  def current_card
+    @deck.cards.shift
+  end
+
+  def record_guess(response_hash)
+    array = response_hash.values
+    one_guess = "#{array[0]} of #{array[1]}"
+    @guesses << card_guess = Guess.new(one_guess, current_card)
+    card_guess
   end
 end
