bbd84d0e87f74ded5ebe5f7a191908865a39616e
Jordan Whitten <jordanwa1947@gmail.com>
2018-08-19 15:31:22 -0700

    first successful insertion sort

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Deck
   attr_reader :cards
   def initialize(cards = [])
@@ -7,4 +8,22 @@ class Deck
   def count
     @cards.length
   end
+
+  def sort
+    #insertion sort
+    array = [9,4,6,7,2,5,16]
+    (array.length - 1).times do
+      x = 1
+      (array.length - 1).times do
+        if array[x] < array[x - 1]
+          array[x], array[x - 1] = array[x - 1], array[x]
+        end
+      x += 1
+      end
+    end
+    array
+  end
 end
+
+deck = Deck.new
+p deck.sort
