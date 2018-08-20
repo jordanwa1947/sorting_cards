b63b77ab02a7e5f4e9eaba9100408962e836ef72
Jordan Whitten <jordanwa1947@gmail.com>
2018-08-20 14:52:28 -0700

    pull error corrections

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,56 +9,49 @@ class Deck
   end
 
   def sort
-    convert_to_sort
+  convert_to_sort
     (cards.length - 1).times do
       x = 1
       (cards.length - 1).times do
         if cards[x].value == cards[x - 1].value
           if cards[x].suit < cards[x - 1].suit
-      (array.length - 1).times do
-        x = 1
-        (cards.length - 1).times do
-          if cards[x].value == cards[x - 1].value
-            if cards[x].suit > cards[x - 1].suit
-              cards[x], cards[x - 1] = cards[x - 1], cards[x]
-            end
-          end
-          if cards[x].value < cards[x - 1].value
             cards[x], cards[x - 1] = cards[x - 1], cards[x]
           end
-        x += 1
         end
+        if cards[x].value < cards[x - 1].value
+          cards[x], cards[x - 1] = cards[x - 1], cards[x]
+        end
+      x += 1
       end
-      convert_back
     end
+    convert_back
+  end
 
-    def convert_to_sort
-      array = []
-      @cards.each do |card|
-        if card.value == "Ace"
-          array << card.value = "Lace"
-        elsif  card.value == "Queen"
-          array << card.value = "Keen"
-        else
-          array << card
-        end
-        array
+  def convert_to_sort
+    array = []
+    @cards.each do |card|
+      if card.value == "Ace"
+        array << card.value = "Lace"
+      elsif  card.value == "Queen"
+        array << card.value = "Keen"
+      else
+        array << card
       end
-    end
-
-    def convert_back
-      array = []
-      @cards.each do |card|
-        if card.value == "Lace"
-          array << card.value = "Ace"
-        elsif  card.value == "Keen"
-          array << card.value = "Queen"
-        else
-          array << card
-        end
         array
-      end
-    end
-    array
     end
   end
+
+  def convert_back
+    array = []
+    @cards.each do |card|
+      if card.value == "Lace"
+        array << card.value = "Ace"
+      elsif  card.value == "Keen"
+        array << card.value = "Queen"
+      else
+        array << card
+      end
+      array
+    end
+  end
+end
