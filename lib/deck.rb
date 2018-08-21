64fc4162101fa9668220af977d26572169502438
Jordan Whitten <jordanwa1947@gmail.com>
2018-08-21 11:05:38 -0700

    flag added to track swaps and new changes to tests

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,23 +1,27 @@
+require 'pry'
 class Deck
   attr_reader :cards
   def initialize(cards)
     @cards = cards
+    @swap = false
   end
 
   def count
     cards.length
   end
-# insertion sort
+
   def sort
     convert_to_sort
     (cards.length - 1).times do
+      @swap = false
       x = 1
-      first_round(x)
+      one_round(x)
+      break if @swap == false
     end
-    convert_back
+    revert_back
   end
 
-  def first_round(x)
+  def one_round(x)
     (cards.length - 1).times do
       same_value_cards(x)
       different_value_cards(x)
@@ -29,6 +33,7 @@ class Deck
     if cards[x].value == cards[x - 1].value
       if cards[x].suit < cards[x - 1].suit
         cards[x], cards[x - 1] = cards[x - 1], cards[x]
+        @swap = true
       end
     end
   end
@@ -36,31 +41,33 @@ class Deck
   def different_value_cards(x)
     if cards[x].value < cards[x - 1].value
       cards[x], cards[x - 1] = cards[x - 1], cards[x]
+      @swap = true
     end
   end
 
   def convert_to_sort
-    cards.map do |card|
+    array = []
+    @cards.each do |card|
       if card.value == "Ace"
-        card.value = "Lace"
-      elsif card.value == "Queen"
-        card.value = "Keen"
+        array << card.value = "Lace"
+      elsif  card.value == "Queen"
+        array << card.value = "Keen"
       else
-        card.value
+        array << card
       end
     end
   end
 
-  def convert_back
-    cards.map do |card|
+  def revert_back
+    array = []
+    @cards.each do |card|
       if card.value == "Lace"
-        card.value = "Ace"
-      elsif card.value == "Keen"
-        card.value = "Queen"
+        array << card.value = "Ace"
+      elsif  card.value == "Keen"
+        array << card.value = "Queen"
       else
-        card.value
+        array << card
       end
     end
-  return cards
   end
 end
